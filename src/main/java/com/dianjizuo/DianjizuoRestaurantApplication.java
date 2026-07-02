85fb772c6bab43b2fef584c981bc85b182307e33
刘晋羽 <570836273@qq.com>
2026-07-02 10:48:09 +0800

    Initial commit: 点即做餐厅项目骨架

---
+++ src/main/java/com/dianjizuo/DianjizuoRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.dianjizuo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DianjizuoRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DianjizuoRestaurantApplication.class, args);
+    }
+
+}
